46c30d4735551549a6d3497eb09da6457b841135
Darsheel-Github <88372039+Darsheel-Github@users.noreply.github.com>
2022-03-08 11:23:19 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    }, 
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does COBOL stand for?",
+        answer: "Common Business Oriented Language",
+        options: [
+            "Coding Business Oriented Language",
+            "Common Business Oriented Language",
+            "Cost Business Oriented Language",
+            "Cross Business Oriented Language"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does AJAX stand for?",
+        answer: "Asynchronous JavaScript And XML",
+        options: [
+            "Amsterdam JavaScript And XML",
+            "Asynchronous JADE And XML",
+            "Asynchronous JavaScript And XML",
+            "American JavaScript And XML"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the correct code for typing text in Python",
+        answer: "print(your text)",
+        options: [
+            "write(your text)",
+            "copy(your text)",
+            "type(your text)",
+            "print(your text)"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which is the most commonly used Code-Editor?",
+        answer: "Visual Studio Code",
+        options: [
+            "Visual Studio Code",
+            "Brackets",
+            "Atom",
+            "Sublime Text"
+        ]
+    },
+    {
+        numb: 10,
+        question: "In which language do we use curly brackets?",
+        answer: "CSS",
+        options: [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "Lua"
+        ]
+    },
+];
